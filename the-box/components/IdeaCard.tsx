e4ec5f56c5693a5e3465abc2f5c5df3d34adef42
Patriciakat <patricija.katinaite@gmail.com>
2025-11-30 02:43:07 +0100

    When adding new ideas, the newly added idea show Now instead of 0h ago

--- the-box/components/IdeaCard.tsx
+++ the-box/components/IdeaCard.tsx
@@ -41,16 +41,16 @@ const IdeaCard: React.FC<Props> = ({
     const date = new Date(dateString);
     const now = new Date();
     const diffMs = now.getTime() - date.getTime();
+    const diffMinutes = Math.floor(diffMs / (1000 * 60));
     const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
     const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
 
-    if (diffHours < 24) {
-      return `${diffHours}h ago`;
-    } else if (diffDays === 1) {
-      return "1 day ago";
-    } else {
+    if (diffMinutes < 1) return "Now";
+    if (diffHours < 1) return `${diffMinutes}m ago`;
+    if (diffHours < 24) return `${diffHours}h ago`;
+    if (diffDays === 1) return "1 day ago";
+    
       return `${diffDays} days ago`;
-    }
   };
 
   const capitalizeStatus = (status: string) => {
